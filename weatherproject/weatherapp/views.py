aff160b0e15edf6de6a4b00a4826ce4d38c5106f
KacperK <kkluge@adva.com>
2024-04-16 13:42:06 +0200

    weather app project

---
+++ weatherproject/weatherapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+import datetime
+
+# Create your views here.
+def index(request):
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Amsterdam'
+
+    appid = '9aa9620e9a0ded2f3adbc1a8f053a855'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city, 'appid' : appid, 'units':'metric'}
+    r = requests.get(url, params=params)
+    response = r.json()
+    description = response['weather'][0]['description']
+    icon = response['weather'][0]['icon']
+    date = datetime.date.today()
+    temperature = response['main']['temp']
+    return render(request, 'weatherapp/index.html',
+                  {'description':description,
+                   'icon':icon,
+                   'temperature':temperature, 'date': date, 'city': city})
